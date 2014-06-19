7f3830b20c73eedb184552f154d67603de833a4f
Manohar <manohar@Manohars-MacBook-Pro.local>
2014-06-19 20:19:44 +0530

    worked on routing

--- app.rb
+++ app.rb
@@ -3,6 +3,16 @@ require "sinatra/base"
 class App < Sinatra::Base
 
 get "/" do
-	"hello world!"
+	"hello world! from get"
 	end
+post "/" do
+	"hello world! in post"
+	end
+put "/" do
+	"hello world! in update"
+	end
+delete "/" do
+	"hello world! in delete"
+	end
+
 end
